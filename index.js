ebaf83f07a81b611132706c490452250cf9fb33d
dikey0ficial <79040270950@ya.ru>
2022-10-26 00:26:48 +0300

    added theme saving

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 document.addEventListener("DOMContentLoaded", on_ready);
 
 function on_ready() {
-	if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+	if (localStorage.getItem("is_dark") === null) {
+		localStorage.setItem("is_dark", window.matchMedia("(prefers-color-scheme: dark)").matches.toString());
+	}
+
+	console.log("init: ", localStorage.getItem("is_dark"))
+	
+	//                                              idk why, but without `== true` it doesn't work  vvvv
+	if (window.matchMedia("(prefers-color-scheme: dark)").matches && localStorage.getItem("is_dark") == "true") {
 		toggle_theme();
 	}
+	
+	console.log("inited: ", localStorage.getItem("is_dark"))
 }
 
 function toggle_theme() {
 	document.body.classList.toggle("dark-theme");
+	localStorage.setItem("is_dark", document.body.classList.contains("dark-theme"))
+	
+	console.log("toggled: ", localStorage.getItem("is_dark"))
 }
